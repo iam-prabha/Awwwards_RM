51217d6eb447b5f53adbe0e6287915ea06a8dec4
iam-prabha <dev.vpcode@gmail.com>
2024-12-13 15:37:52 +0530

    complete

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Button = ({ id, title, containerClass, leftIcon, rightIcon }) => {
+  return (
+    <>
+      <button
+        id={id}
+        className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 ${containerClass}`}
+      >
+        {leftIcon}
+        <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+          <div>{title}</div>
+        </span>
+        {rightIcon}
+      </button>
+    </>
+  );
+};
+export default Button;
